e8f9f5ede1732133dfa280c60fd18e15aa535a17
marsk7 <102530097+marsk7@users.noreply.github.com>
2025-08-06 19:14:45 +1000

    Update template.php
    add web logo

--- app/Views/template.php
+++ app/Views/template.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Eat Now</title>
+    <link rel="icon" type="image/png" href="/favicon.ico">
     <!-- This is the main stylesheet for Bootstrap. It includes all the CSS necessary for Bootstrap's components and utilities to work. -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Include Bootstrap Icons -->
